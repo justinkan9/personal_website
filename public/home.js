d9fb1e5c219fcbcc3eca003a3f5b53a0283233e6
Justin S Kang <justinkan9@users.noreply.github.com>
2016-08-14 23:12:02 -0700

    Change about me page to icons and replace future pages with under construction, fix contact buttons panel

--- public/home.js
+++ public/home.js
@@ -50,10 +50,26 @@ var AboutMePanel = React.createClass({
             <div id="about-me-parallax" className="parallax__group">
                 <div className="parallax__layer parallax__layer--base"/>
                 <div className="parallax__layer parallax__layer--fore">
-                    <img id="logo-img" src="public/img/jkang.svg"/>
-                    <h3 className="name-label">Justin   Seokhyun   Kang</h3>
-                    <i className="fa fa-angle-down fa-4x scroll-down-img"/>
+                    <h1 className="title-top">about</h1>
+                    <div className="container">
+                        <AboutTile labelText="run" imgPath="public/img/forest.svg"/>
+                        <AboutTile labelText="design" imgPath="public/img/design.svg"/>
+                        <AboutTile labelText="code" imgPath="public/img/code.svg"/>
+                        <AboutTile labelText="love" imgPath="public/img/heart.svg"/>
+                    </div>
                 </div>
+            </div>
+        );
+    }
+});
+
+
+var AboutTile = React.createClass({
+    render: function() {
+        return(
+            <div className="col-xs-6 col-sm-3 about-tile">
+                <img src={this.props.imgPath}/>
+                <p className="tile-label">{this.props.labelText}</p>
             </div>
         );
     }
@@ -66,8 +82,7 @@ var PortfolioPanel = React.createClass({
                 <div className="parallax__layer parallax__layer--back"/>
                 <div className="parallax__layer parallax__layer--base">
                     <img id="logo-img" src="public/img/jkang.svg"/>
-                    <h3 className="name-label">Justin   Seokhyun   Kang</h3>
-                    <i className="fa fa-angle-down fa-4x scroll-down-img"/>
+                    <h3 className="name-label">Under Construction</h3>
                 </div>
             </div>
         );
@@ -80,13 +95,42 @@ var ContactsPanel = React.createClass({
             <div id="contacts-parallax" className="parallax__group">
                 <div className="parallax__layer parallax__layer--base">
                     <img id="logo-img" src="public/img/jkang.svg"/>
-                    <h3 className="name-label">Justin   Seokhyun   Kang</h3>
-                    <i className="fa fa-angle-down fa-4x scroll-down-img"/>
+                    <h3 className="name-label">Under Construction</h3>
                 </div>
+                <ContactsButtonsPanel/>
             </div>
         );
     }
 });
+
+var ContactsButtonsPanel = React.createClass({
+    render: function() {
+        return(
+            <div id="contacts-button-panel" className="bar-panel contacts-background">
+                <div className="clouds"/>
+                <ContactButton contactLink="mailto:justinkan9@yahoo.com" contactImg="fa-envelope" contactColor="#9EC44F"/>
+                <ContactButton contactLink="https://www.linkedin.com/in/justin-kang-a4b5125b" contactImg="fa-linkedin" contactColor="#F1822D"/>
+                <ContactButton contactLink="https://github.com/justinkan9" contactImg="fa-github" contactColor="#BE2481"/>
+                <ContactButton contactLink="https://www.facebook.com/justinokang" contactImg="fa-facebook" contactColor="#427BA6"/>
+            </div>
+        );
+    }
+});
+
+var ContactButton = React.createClass({
+    render: function() {
+        var style = {
+            color: this.props.contactColor
+        };
+        return(
+            <a style={style} href={this.props.contactLink} target="_blank" className="circle-link">
+                <i className={"fa fa-4x " + this.props.contactImg}/>
+            </a>
+        );
+    }
+});
+
+
 
 ReactDOM.render(
     <HomePage/>,
